6e4f35be9ec45e5728e110e67a61e8f427f3cf28
Shea Schwenn <sheaschwenn@gmail.com>
2023-03-29 20:37:43 -0700

    created two arrays to concat into one password array, working on getting password random

--- Develop/script.js
+++ Develop/script.js
@@ -17,19 +17,16 @@ var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"
 var uppercase = lowercase.map(lowercase =>lowercase.toUpperCase())  
 var numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 var special;
-
+var needed = [];
+var temp=[];
 var password = [];
-
-
-console.log(special)
+var possible = [];
+var character;
 
 
 var generateBtn = document.querySelector("#generate");
 
-function randomCharacter(array){
-  var character = array[Math.floor(Math.random()*array.length)];
-  return character;
-    }
+
 
 
 
@@ -54,7 +51,7 @@ generateBtn.addEventListener("click", writePassword);
 function userChoice(){ 
 
         // length has stored the password length as an integer
-  var length = parseInt(prompt("Password Length"));
+      var length = parseInt(prompt("Password Length"));
       if(length >= 8 && length <=128){
       console.log(length); 
       }
@@ -87,13 +84,11 @@ function userChoice(){
     numeric : confirmNumeric,
     special : confirmSpecial,
    }
+   return userAnswer
   }
 
   
-   function randomGen(){
-    var random = Math.floor(Math.random()*length)
-    return random
-   }
+  
    
  
     
@@ -105,34 +100,75 @@ function userChoice(){
 
 
   
-  
+   function randomCharacter(array){
+    character = array[Math.floor(Math.random()*array.length)];
+    return character;
+   }
 
  
  
   function generatePassword(){
     var userInfo = userChoice();
-    for(length; length>0; length--){
+    console.log(userInfo);
+    if(userInfo.lowercase){
+      needed.push(randomCharacter(lowercase));
+    //  length = length-1
+
+    // pick this many characters from array at random
+   }
+   if(userInfo.uppercase){
+    needed.push(randomCharacter(uppercase));
+    // length = length-1
+   }
+   if(userInfo.numeric){
+    needed.push(randomCharacter(numeric));
+    // length = length-1
+   }
+  
+   if(userInfo.special){
+    needed.push(randomCharacter(special)); 
+    // length = length -1 
+    }
+    for(var i = userInfo.length; i>0; i--){
     
-      if( confirmLowercase){
-         password.push(randomCharacter(lowercase));
+      if(userInfo.lowercase){
+         possible.push(randomCharacter(lowercase));
+        
+
        // pick this many characters from array at random
       }
-      if(confirmUppercase){
-       randomCharacter(uppercase);
+      if(userInfo.uppercase){
+       possible.push(randomCharacter(uppercase));
+       
       }
-      if(confirmNumeric){
-       randomCharacter(numeric);
+      if(userInfo.numeric){
+       possible.push(randomCharacter(numeric));
+       
       }
      
-      if(confirmSpecial){
-       randomCharacter(special); 
+      if(userInfo.special){
+       possible.push(randomCharacter(special)); 
        
        }
-       
+      password = needed.concat(possible);
+      password.length = userInfo.length; 
+       function randomPassword(password){
+        for(var j = password.length; j>0;j--){
+          random = Math.floor(Math.random()*j);
+          [password[password.length], password[j]] = [password[j],password[password.length]];
+        }
+        return password
+       }
+       randomPassword(password);
+   
+       }
+      return password.join("")
+      
       }
-    }
-    generatePassword();
-    console.log(password)
+    // console.log(password)
+    // console.log(possible)
+    // console.log(userInfo.length)
+    
   
     
       
