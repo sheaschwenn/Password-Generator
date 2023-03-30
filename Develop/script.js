5ff631e9322ef4bda1e8c05b9861d52506c9772c
Shea Schwenn <sheaschwenn@gmail.com>
2023-03-29 21:18:24 -0700

    added special characters array

--- Develop/script.js
+++ Develop/script.js
@@ -16,7 +16,7 @@
 var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var uppercase = lowercase.map(lowercase =>lowercase.toUpperCase())  
 var numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-var special;
+var special = ['"',"'","!","#","$","%","&","(",")","*","+",",","-",".","/",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~","\\"] ;
 
 
 
